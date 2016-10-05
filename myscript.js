2c023b10240df892dc273d26b33c376d8d648a3d
NetPlayer <netplayer@gmx.com>
2016-10-05 21:45:53 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+    $('#pagelet_bluebar').css({
+
+    	'position':'fixed',
+    	'z-index':'9999999';
+
+    });
+$('#mailview-bottom').remove();
+//$('#topnav').remove();
+//$('#rcenvelope').removeClass('rcenvelopeImg');
+//$('#portal-header').remove();
+//$('#rcenvelope').css('top','0');
+    
+});
+
+
