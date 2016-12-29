226d1a27131c5c1cfec436a7fab656bca90d9e91
NetPlayer <netplayer@gmx.com>
2016-12-29 15:10:50 +0200

    Script update
    Tightened top bar control on scroll and cookie notification close link. Main function added.

--- myscript.js
+++ myscript.js
@@ -1,12 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function(){  
     
-    $('#pagelet_bluebar').css({
+	   
+    //sometimes cookies notification close link resets bar position
+   $('#u_0_2').click(function(){ 	   
+	  topbar(); 
+   });
+    
+    //actual hack function
+	function topbar(){
+    $('#pagelet_bluebar').css({'z-index':'9999999','position':'fixed'});
+  
+    }
+    
+    //run script on page scroll just in case
+    window.onscroll = function() {
+    topbar();
+    }
 
-    	'position':'fixed !important',
-    	'z-index':'9999999';
-
-    });
-
+    topbar();
     
 });
 
